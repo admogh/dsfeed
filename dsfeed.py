eedb2345c0ff9ae14e7689d4a0c6a784ecea20e8
admo <admoifgm@gmail.com>
2022-10-25 22:59:56 +0900

    issues/26 introduce sync interval minutes

--- dsfeed.py
+++ dsfeed.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 import os
 import inspect
 from lxml import html as lxmlhtml
@@ -6,6 +7,8 @@ import html
 from lxml import etree
 from urllib.parse import urlparse
 import emoji 
+import time
+import common_library
 
 class DsFeed:
     @staticmethod
@@ -62,11 +65,35 @@ class DsFeed:
                 e = ret.group(2).strip()
         return cls.getString(e)
 
-    def __init__(self, cdriver):
+    def __del__(self):
+        print("DsFeed.__del__")
+        self.cmn.scpPutFile(self.shostname, self.dbpath, self.spath, ow=False)
+        self.cdriver.driver.quit()
+        del self.dbm
+        del self.cmn
+
+    def __init__(self, dbm, cdriver, basedir, rdbpath):
+        self.dbm = dbm
         self.cdriver = cdriver
         self.driver = cdriver.driver
+        self.cut = time.time()
+        try:
+          sim = int(os.getenv('SYNC_INTERVAL_MINUTES'))
+        except Exception as ex:
+          print("catch Exception to get SYNC_INTERVAL_MINUTES, set to 20 minutes default:", ex)
+          sim = 20
+        self.sim = sim
+        print("sim:",sim)
+        self.basedir = basedir
+        self.dbpath =  self.basedir + "/dsfeed.db"
+        self.shostname = os.getenv('SYNC_HOSTNAME')
+        self.spath = os.getenv('SYNC_PATH')
+        self.cmn = common_library.CommonLibrary(self.shostname)
+        if rdbpath != "":
+          print("overwrite db(rdbpath,syncpath):",rdbpath,self.spath)
+          self.cmn.scpPutFile(self.shostname, rdbpath, self.spath)
+        self.cmn.scpGetFile(self.shostname, self.spath, self.dbpath)
 
-    #@classmethod
     def getSiteList(self, src, ui):
         curl = self.driver.current_url
         print("curl:",curl)
@@ -124,3 +151,87 @@ class DsFeed:
           return urls[sui[0]]
         except IndexError:
           return []
+
+    def saveDriver(self, fn):
+        logdir = os.getenv('LOG_DIR', self.basedir+"/log")
+        if not os.path.exists(logdir):
+          return
+        dt = datetime.now().strftime("%Y%m%d%H%M%S")
+        path = logdir + "/" + fn + "___" + dt
+        try:
+          self.driver.save_screenshot(path + ".png")
+          with open(path + ".html", "w") as f:
+              f.write(self.driver.page_source)
+        except Exception as ex:
+          print("catch Exception in saveDriver:",ex)
+
+    def srcScrape(self, srcurl, doc, ui=0):
+        doc = html.unescape(doc)
+        doc = ' '.join(doc.splitlines())
+        doc = doc.replace("<![CDATA[", "").replace("]]>", "")
+        # title
+        title = self.getElement(doc, "title")
+        print("title: "+title)
+        urls = self.getSiteList(doc, ui)
+        if len(urls) == 0:
+          print("not found urls:ui:",ui)
+          return
+        curl = self.driver.current_url
+        domain = urlparse(curl).netloc
+        nps = 0
+        for url in urls:
+            ret = re.search("^(\/|\.\/)", url, re.S)
+            if ret:
+                url = "https://" + domain + url
+            print("url:"+url)
+            self.dbm.cur.execute("select count(*) from links where link = ?", (url,))
+            c = self.dbm.cur.fetchone()[0]
+            if c >= 1:
+                continue
+            try:
+                self.driver.get(url)
+            except Exception as e:
+                self.saveDriver(common_library.CommonLibrary.getSrcLocationString())
+                print(e)
+                print("driver get failed and continue:"+url)
+                continue
+            doc = self.driver.page_source
+            tree = lxmlhtml.fromstring(doc)
+            # make out
+            out = "**"+title+"**\n"
+            etitle = self.getElement(doc, "title")
+            self.dbm.cur.execute("select count(*) from titles where title = ?", (etitle,))
+            c = self.dbm.cur.fetchone()[0]
+            if c >= 1:
+                continue
+            edesc = self.getElement(doc, "description")
+            if edesc == "":
+                edesc = self.getElement(doc, "h1")
+            print(etitle,edesc)
+            if edesc != "" and etitle != "":
+                out = out + etitle + "\n" + edesc + "\n"
+                out = out + url + "\n"
+                print(out)
+                common_library.CommonLibrary.toDiscord(os.getenv('DISCORD_WEBHOOK_URL'), out)
+                # datetime
+                cd = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                # insert
+                self.dbm.cur.execute(
+                    "insert into links(source_url, link, created, updated) \
+                            values (?, ?, ?, ?)",
+                    (srcurl, url, cd, cd),
+                )
+                self.dbm.cur.execute(
+                    "insert into titles(source_url, title, created, updated) \
+                            values (?, ?, ?, ?)",
+                    (srcurl, etitle, cd, cd),
+                )
+                self.dbm.conn.commit()
+                nps = nps + 1
+                nut = time.time()
+                if nut > self.cut+self.sim*60:
+                  self.cmn.scpPutFile(self.shostname, self.dbpath, self.spath, ow=False)
+                  self.cut = nut
+        if nps == 0:
+          self.srcScrape(srcurl, doc, ui+1)
+
